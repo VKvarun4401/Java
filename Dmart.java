ff56f4af5de46233724b14c2dc7cf7578849ae09
vkvarun <vkvarun4401@gmail.com>
2024-06-21 11:30:05 +0530

    new file

---
+++ Dmart.java
@@ -0,0 +1,36 @@
+class Dmart {
+   static String chocolates[] = {"kit kat", "Dairy Milk", "Snickers", "Milky bar"};
+   static String perfumesName[] = {"Bella Vita Luxury", "Plum", "Fogg", "Plum"};
+   static String fruits[] = {"Citrus Fruits", "Berries", "Mango", "Pineapple"};
+   static String vegetables[] = {"Potato", "Tomato", "Radish", "Lady’s finger"};
+   static String clothings[] = {"Nike", "Adidas", "H&M", "Burberry"};
+   static String sandals[] = {"Slide sandals", "Gladiator sandals", "Flip flops", "Strappy sandals"};
+   static String hairOils[] = {"Indulekha Bhringa", "Soulflower Rosemary Lavender", "Morrocanoil", "Mamaearth Onion Hair Oil"};
+   static String juiceNames[] = {"Maaza", "Sprite", "Coca-Cola", "Slice"};
+   static String kitchenItems[] = {"Knives", "Cookware", "Dinnerware", "Kitchen Tools"};
+   static String bags[] = {"carpet bag", "Duffel bag", "Hobo bag", "Kelly bag"};
+   static String shampooBrands[] = {"Himalaya", "Clinic Plus", "Patanjali", "Indulekha"};
+   static String cookies[] = {"Piped cookies", "Roll-out cookies", "Butter cookies", "chocolate chips"};
+   static String iceCream[] = {"Amul", "Arun", "Ideal", "Natural"};
+   static String pickles[] = {"Mix-Vegetables", "Mango", "Lemon", "ChiliCrunch"};
+   static String kidsToys[] = {"bicycle", "train", "doll", "rubber ducky"};
+   public static void main(String[] newCollection) {
+     System.out.println("The Avaiable Chocolates are: \n"+ chocolates[0]+"\n"+ chocolates[1]+"\n"+ chocolates[2]+"\n"+ chocolates[3]+"\n");
+     System.out.println("The Avaiable Perfumes are:");
+	 System.out.println(perfumesName[1]);
+	 System.out.println(perfumesName[0]);
+	 System.out.println("The types of Fruits are: \n"+ fruits[0]+"\n"+ fruits[1]+"\n"+ fruits[2]+"\n"+ fruits[3]+"\n");
+	 System.out.println("The types of vegetables are: \n"+ vegetables[0]+"\n"+ vegetables[1]+"\n"+ vegetables[2]+"\n"+ vegetables[3]+"\n");
+	 System.out.println("The Avaiable Clothing Brand Names are: \n"+ clothings[0]+"\n"+ clothings[1]+"\n"+ clothings[2]+"\n"+ clothings[3]+"\n");
+	 System.out.println("The different types of Sandals are: \n"+ sandals[0]+"\n"+ sandals[1]+"\n"+ sandals[2]+"\n"+ sandals[3]+"\n");
+	 System.out.println("The Avaiable Hair Oils are: \n"+ hairOils[0]+"\n"+ hairOils[1]+"\n"+ hairOils[2]+"\n"+ hairOils[3]+"\n");
+	 System.out.println("The Avaiable Juice Names are: \n"+ juiceNames[0]+"\n"+ juiceNames[1]+"\n"+ juiceNames[2]+"\n"+ juiceNames[3]+"\n");
+	 System.out.println("The Kitchen Items are: \n"+ kitchenItems[0]+"\n"+ kitchenItems[1]+"\n"+ kitchenItems[2]+"\n"+ kitchenItems[3]+"\n");
+	 System.out.println("The Avaiable Bags are: \n"+ bags[0]+"\n"+ bags[1]+"\n"+ bags[2]+"\n"+ bags[3]+"\n");
+	 System.out.println("The Avaiable Shampoo Brands are: \n"+ shampooBrands[0]+"\n"+ shampooBrands[1]+"\n"+ shampooBrands[2]+"\n"+ shampooBrands[3]+"\n");
+	 System.out.println("The types of cookies are: \n"+ cookies[0]+"\n"+ cookies[1]+"\n"+ cookies[2]+"\n"+ cookies[3]+"\n");
+	 System.out.println("The Avaiable iceCream Brands are: \n"+ iceCream[0]+"\n"+ iceCream[1]+"\n"+ iceCream[2]+"\n"+ iceCream[3]+"\n");
+	 System.out.println("The types of  pickles are: \n"+ pickles[0]+"\n"+ pickles[1]+"\n"+ pickles[2]+"\n"+ pickles[3]+"\n");
+	 System.out.println("The Avaiable kids Toys are: \n"+ kidsToys[0]+"\n"+ kidsToys[1]+"\n"+ kidsToys[2]+"\n"+ kidsToys[3]+"\n");
+        }
+    }
